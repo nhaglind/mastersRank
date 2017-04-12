ddb6f7456cdb7f7d94d0c421654e4d7b57e9c12f
Nick Haglind <nhaglind@gmail.com>
2017-04-12 11:43:17 -0500

    Add loop to grab entire list and change string to int when needed

--- getLeaderboard.py
+++ getLeaderboard.py
@@ -4,21 +4,25 @@ from bs4 import BeautifulSoup
 
 page = requests.get("http://www.espn.com/golf/leaderboard?tournamentId=2700")
 soup = BeautifulSoup(page.content, 'html.parser')
-full_name = soup.find_all('a', class_ = 'full-name')[0].get_text()
-under_over = int(soup.find_all('td', class_ = 'relativeScore')[0].get_text())
-round_1 = int(soup.find_all('td', class_ = 'round1 in post')[0].get_text())
-round_2 = int(soup.find_all('td', class_ = 'round2 in post')[0].get_text())
-round_3 = int(soup.find_all('td', class_ = 'round3 in post')[0].get_text())
-round_4 = int(soup.find_all('td', class_ = 'round4 in post')[0].get_text())
-total_score = int(soup.find_all('td', class_ = 'totalScore in post')[0].get_text())
 
-print(full_name, round_1, round_2, round_3, round_4, total_score, under_over)
+
+ary = []
+name_array = soup.find_all('a', class_ = 'full-name')
+uo_array = soup.find_all('td', class_ = 'relativeScore')
+ts_array = soup.find_all('td', class_ = 'totalScore in post')
+length = len(name_array)
+
+for i in range(0, length):
+    full_name = name_array[i].get_text()
+    if (uo_array[i].get_text() == 'E' or (uo_array[i].get_text() == 'CUT')):
+        under_over = uo_array[i].get_text()
+    else:
+        under_over = int((uo_array[i].get_text()))
+    total_score = int((ts_array[i].get_text()))
+    ary.append({'playerName': full_name, 'score': total_score, 'overUnder': under_over})
+print(ary)
 
 file = open("leaderboard_data.json", "w")
-output = {
-    "playerName": (full_name),
-    "score": (total_score),
-    "underOver": (under_over)
-}
+output = ary
 json.dump(output, file)
 file.close()
